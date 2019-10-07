9f2035ef4905b8e47e65ae4e5304026792826301
aravie <raviee@gmx.de>
2019-10-07 21:08:23 +0200

    myos7 Driver Abstractions

--- Makefile
+++ Makefile
@@ -1,12 +1,20 @@
 
-GPPPARAMS = -m32 -fno-use-cxa-atexit -nostdlib -fno-builtin -fno-rtti -fno-exceptions -fno-leading-underscore -Wno-write-strings
+# sudo apt-get install g++ binutils libc6-dev-i386
+# sudo apt-get install VirtualBox grub-legacy xorriso
+
+GCCPARAMS = -m32 -fno-use-cxa-atexit -nostdlib -fno-builtin -fno-rtti -fno-exceptions -fno-leading-underscore -Wno-write-strings
 ASPARAMS = --32
 LDPARAMS = -melf_i386
 
-objects = loader.o gdt.o port.o interruptstubs.o interrupts.o keyboard.o mouse.o kernel.o
+objects = loader.o gdt.o driver.o port.o interruptstubs.o interrupts.o keyboard.o mouse.o kernel.o
+
+
+run: mykernel.iso
+	(killall VirtualBox && sleep 1) || true
+	VirtualBox --startvm 'My Operating System' &
 
 %.o: %.cpp
-	g++ $(GPPPARAMS) -o $@ -c $<
+	gcc $(GCCPARAMS) -c -o $@ $<
 
 %.o: %.s
 	as $(ASPARAMS) -o $@ $<
@@ -14,31 +22,24 @@ objects = loader.o gdt.o port.o interruptstubs.o interrupts.o keyboard.o mouse.o
 mykernel.bin: linker.ld $(objects)
 	ld $(LDPARAMS) -T $< -o $@ $(objects)
 
-install: mykernel.bin
-	sudo cp $< /boot/mykernel.bin
-
 mykernel.iso: mykernel.bin
 	mkdir iso
 	mkdir iso/boot
 	mkdir iso/boot/grub
-	cp $< iso/boot/
-	echo 'set timeout=0' >> iso/boot/grub/grub.cfg
-	echo 'set default=0' >> iso/boot/grub/grub.cfg
-	echo '' >> iso/boot/grub/grub.cfg
+	cp mykernel.bin iso/boot/mykernel.bin
+	echo 'set timeout=0'                      > iso/boot/grub/grub.cfg
+	echo 'set default=0'                     >> iso/boot/grub/grub.cfg
+	echo ''                                  >> iso/boot/grub/grub.cfg
 	echo 'menuentry "My Operating System" {' >> iso/boot/grub/grub.cfg
-	echo ' multiboot /boot/mykernel.bin' >> iso/boot/grub/grub.cfg
-	echo ' boot' >> iso/boot/grub/grub.cfg
-	echo '}' >> iso/boot/grub/grub.cfg
-	grub-mkrescue --output=$@ iso
+	echo '  multiboot /boot/mykernel.bin'    >> iso/boot/grub/grub.cfg
+	echo '  boot'                            >> iso/boot/grub/grub.cfg
+	echo '}'                                 >> iso/boot/grub/grub.cfg
+	grub-mkrescue --output=mykernel.iso iso
 	rm -rf iso
-	
-run: mykernel.iso
-	(killall VirtualBox && sleep 1) || true
-	VirtualBox --startvm "My Operating System" &
-	
+
 install: mykernel.bin
 	sudo cp $< /boot/mykernel.bin
-	
+
 .PHONY: clean
 clean:
 	rm -f $(objects) mykernel.bin mykernel.iso
--- mouse.cpp
+++ mouse.cpp
@@ -1,19 +1,44 @@
 
 #include "mouse.h"
 
-    MouseDriver::MouseDriver(InterruptManager* manager)
+    MouseEventHandler::MouseEventHandler()
+    {
+
+
+    }
+
+    void MouseEventHandler::OnActivate()
+    {
+    }
+    void MouseEventHandler::OnMouseDown(uint8_t button)
+    {
+
+    }
+    void MouseEventHandler::OnMouseUp(uint8_t button)
+    {
+
+    }
+    void MouseEventHandler::OnMouseMove(int x, int y)
+    {
+
+    }
+
+    MouseDriver::MouseDriver(InterruptManager* manager, MouseEventHandler* handler)
     : InterruptHandler(manager, 0x2C),
     dataport(0x60),
     commandport(0x64)
     {
-        uint16_t* VideoMemory = (uint16_t*)0xb8000;
+	this->handler = handler;
+    }
+
+    MouseDriver::~MouseDriver()
+    {
+    }
+
+    void MouseDriver::Activate()
+    {
         offset = 0;
         buttons = 0;
-        x = 40;
-        y = 12;
-        VideoMemory[80*y+x] = (VideoMemory[80*y+x] & 0x0F00) << 4
-                            | (VideoMemory[80*y+x] & 0xF000) >> 4
-                            | (VideoMemory[80*y+x] & 0x00FF);
 
         commandport.Write(0xA8);
         commandport.Write(0x20); // command 0x60 = read controller command byte
@@ -25,10 +50,6 @@
         dataport.Write(0xF4);
         dataport.Read();
     }
-
-    MouseDriver::~MouseDriver()
-    {
-    }
     
     uint32_t MouseDriver::HandleInterrupt(uint32_t esp)
     {
@@ -37,27 +58,18 @@
             return esp;
 
         buffer[offset] = dataport.Read();
-        offset = (offset + 1) % 3;
+
+	if(handler == 0)
+		return esp;
+
+       offset = (offset + 1) % 3;
 
         if(offset == 0)
         {
             if(buffer[1] != 0 || buffer[2] != 0)
             {
-                static uint16_t* VideoMemory = (uint16_t*)0xb8000;
-                VideoMemory[80*y+x] = (VideoMemory[80*y+x] & 0x0F00) << 4
-                                    | (VideoMemory[80*y+x] & 0xF000) >> 4
-                                    | (VideoMemory[80*y+x] & 0x00FF);
 
-                x += buffer[1];
-                if(x >= 80) x = 79;
-                if(x < 0) x = 0;
-                y -= buffer[2];
-                if(y >= 25) y = 24;
-                if(y < 0) y = 0;
-
-                VideoMemory[80*y+x] = (VideoMemory[80*y+x] & 0x0F00) << 4
-                                    | (VideoMemory[80*y+x] & 0xF000) >> 4
-                                    | (VideoMemory[80*y+x] & 0x00FF);
+		handler->OnMouseMove(buffer[1], buffer[2]);
 
 		/*for(uint8_t i = 0; i < 3; i++)
 		{
@@ -71,19 +83,19 @@
 		buttons = buffer[0];*/
             }
 
-            /*
+            
             for(uint8_t i = 0; i < 3; i++)
             {
                 if((buffer[0] & (0x1<<i)) != (buttons & (0x1<<i)))
                 {
                     if(buttons & (0x1<<i))
-                        handler->OnMouseButtonReleased(i+1);
+                        handler->OnMouseUp(i+1);
                     else
-                        handler->OnMouseButtonPressed(i+1);
+                        handler->OnMouseDown(i+1);
                 }
             }
             buttons = buffer[0];
-            */
+            
         }
         return esp;
     }
